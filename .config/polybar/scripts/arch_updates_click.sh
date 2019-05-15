a646f6a4d41dc6fd5125618b710212c4bc45add5
Yury Kurlykov <sh1ftr@protonmail.ch>
2019-05-15 23:58:57 +1000

    polybar: fix click handler for update counter

--- .config/polybar/scripts/arch_updates_click.sh
+++ .config/polybar/scripts/arch_updates_click.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-gksudo -m 'Enter the password to update package databases' 'pacman -Sy' && pikaur -Qu 2> /dev/null | rofi -dmenu -p "update list" &
+if gksudo -m 'Enter the password to update package databases' 'pacman -Sy'; then
+    pikaur -Qu 2> /dev/null | rofi -dmenu -p "update list" &
+else
+    rofi -e $'An error has occured.\nCheck system logs.'
+fi
